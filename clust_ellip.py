73b1be6c715af320ad76f2ae61974691483a9b62
krishna chaitanya chavati <chaithukrishnazz2@gmail.com>
2014-06-21 03:16:30 +0530

    Initialized clustered ellipsoidal sampler

---
+++ clust_ellip.py
@@ -0,0 +1,47 @@
+"""Implementation of clustered ellipsoidal method for using in multimodal nested sampling as
+an improvement to detect modes in the posterior. This was proposed in multinest paper by Feroz
+and Hobson(2008). This method is closer in spirit to the recursive technique advocated in Shaw
+et al. 
+
+Refernces :
+===========
+
+Multinest paper by Feroz and Hobson 2008.
+Shaw R., Bridges M., Hobson M.P., 2007, MNRAS, in press (astro-ph/0701867)
+
+"""
+
+import numpy as np
+from math import *
+from sources import *
+import random
+
+
+class Clustered_Sampler(object):
+
+	"""Initialize using the information of current active samples and the object to evolve"""
+	def __init__(self, to_evolve, active_samples, likelihood_constraint):
+
+		self.source = to_evolve
+		self.points = active_samples
+		self.LC     = likelihood_constraint
+		self.clustered_point_set = None
+		self.number_of_clusters = None
+		self.ellipsoid_set = None
+
+	"""This method clusters the samples and return the mean points of each cluster"""
+
+	def cluster(self):
+		return None
+
+	"""This method builds ellipsoids enlarged by a factor, around each cluster of active samples
+	 from which we sample to evolve using the likelihood_constraint"""	
+
+	def build_ellipsoids(self):
+		return None
+
+    
+    """This method is responsible for sampling from the enlarged ellipsoids with certain probability
+    The method also checks if any ellipsoids are overlapping and behaves accordingly """
+	def sample(self):
+		return None
