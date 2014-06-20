6c86bacfa01a110aa64d8be976fb920ff1a4b6d7
krishna chaitanya chavati <chaithukrishnazz2@gmail.com>
2014-06-20 20:26:46 +0530

    Added Implementation information and initialization

--- nest.py
+++ nest.py
@@ -1,3 +1,31 @@
+""" Calculating the evidence and the posterior samples using the Multimodal Nested sampling method.
+Multimodal nested sampling by FEROZ and HOBSON is a method built on nested sampling algorithm
+by JOHN SKILLING et al for sampling through posterior with multiple modes. The method uses a loop 
+where we continously replace the point in active samples which has lower likelihood with a point that
+has greater likelihood. The discarded point is a posterior inference and used in calculation of evidence.
+The method has the following steps:
+1)Sample points from prior
+2)Loop under stopping criterion
+  1) Find out the point with lowest likelihood-L(i)
+  2) Assign Prior mass for this iteration-X(i) 
+  3) Set the values of weights using the trapezoidal rule-W(i)
+  4) Increment the evidence by lowest_likelihood- L(i)*W(i) and store the point as posterior inference.
+  5) Check stopping criterion. If converged Increment the evidence by using the active_samples too
+  6) If not converged, sample a point from prior with likelihood greater than L(i)
+3) After reaching maximum number of iterations Increment the evidence by using the active_samples too
+4) Output the Evidence, the posterior samples and other important information 
+The Difficult part in this method is sampling a new point from the prior satisfying the likelihood constraint.
+According to the Multinest paper by Feroz et al. there are some methods that could help restrict the prior 
+volume from which to draw a point ensuring transition between a lower iso-likelihood contour to a higher
+iso-likelihood contour. The methods are :
+1) Clustered ellipsoidal sampling - Builds ellipsoids around midpoints of clustered active_samples and draws
+samples from these ellipsoids with a certain probability assigned to each ellipsoid.
+2) Metropolis nested sampling - Uses a proposal distribution generally a symmetric gaussian distribution with a 
+dispersion value which changes and drives the process to higher likelihood regions as we sample.
+We are going to try both the metropolis nested sampling and clustered ellipsoidal sampling for this approach.
+The Following is a class implementation of Nested_Sampler."""
+
+
 import numpy as np
 from math import *
 from sources import *
@@ -9,7 +37,7 @@ class Nested_Sampler(Object):
 
     """Initialization for the Nested_Sampler"""
 
-    def __init__(self, no_active_samples, max_iter, metropolis=True, cluster=False, plot=False, conv_thresh=0.1):
+    def __init__(self, no_active_samples, max_iter, sample = "metropolis", plot=False, conv_thresh=0.1):
 
         """Number of active_samples in the nested sampling loop to start"""
         self.no_active_samples     = no_active_samples
@@ -17,45 +45,28 @@ class Nested_Sampler(Object):
         """Maximum number of iterations after which the loop is terminated"""
         self.maximum_iterations    = max_iter
         
-        """Use Metropolis Nested sampling""" 
-        self.metropolis            = metropolis
+        """ The sampling method used to draw a new sample satisfying the likelihood constraint"""
+        self.sample                = sample
 
-        """Use Clustered Ellipsoidal sampling"""
-        self.cluster               = cluster
+        """Plot posterior samples while running the loop to check how the method is working"""
         self.plot                  = plot
 
-        """Stopping criterion for nested sample. When the evidence changes by less than this , we stop the process"""
+        """Stopping criterion for nested sample. The same value used in the Multinest paper """
         self.convergence_threshold = 0.1
 
         """ Sampling active points from the prior distribution specified """
         self.active_samples        = get_sources(no_active_samples)
         
-        """Information which helps in calculating the uncertainity in evidence"""
-        self.information           = None
+        """ Variable to hold evidence at each iteration"""
+        self.evidence              = None
 
-        """ Variable to hold log_evidence at each iteration"""
-        self.log_evidence          = None
+        """ Posterior samples for evidence and plotting """
+        self.posterior_inferences  = []
 
-        """A Helper variable that we use in information calculation"""
-        self.logZ_updated          = None
-
-        """Likelihood constraint for each iteration assigned the value of the point with lowest Likelihood"""
-        self.likelihood_constraint = None
-
-        """storing iteration number in case the evidence converges before the maximum number of iterations"""
-        self.iteration             = None
-
-        
+        """ Prior volume which is used to calculate the weight of the point at each iteration"""
+        self.prior_volume          = None
 
     
+    """ Method that runs the main nested sampling loop"""
     
-    def 
-        
-
-
-
-
-
-
-
-
+    def fit(self):  
